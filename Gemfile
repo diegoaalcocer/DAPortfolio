d3e53332c1ca811f183c1624932d60e7c851d46e
Diego Alcocer <diegoaalcocer@gmail.com>
2018-01-31 20:07:31 +0000

    Updated gemfile and declared ruby version

--- Gemfile
+++ Gemfile
@@ -60,3 +60,5 @@ gem 'carrierwave', '~> 1.2', '>= 1.2.2'
 gem 'carrierwave-aws', '~> 1.3'
 gem 'mini_magick', '~> 4.8'
 gem 'gritter', '~> 1.2'
+
+ruby "2.4.1" 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+User.create!(
+  email: "test@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Regular User",
+)
+
+puts "1 regular user created"
+
 9.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
